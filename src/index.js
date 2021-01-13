3c94e6a5ce6da2132b8b8fc39e1ca7add78e61b7
shivakumar901072 <shivakumar901072@gmail.com>
2021-01-13 17:57:40 +0530

    code

--- src/index.js
+++ src/index.js
@@ -11,7 +11,24 @@ const { data } = require('./data')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
-// your code goes here
+
+
+app.get('/topRankings', (req,res)=>{
+    const tempOffset=senitize(req.query.offset)?Number(req.query.offset):0;
+    const tempLimit=senitize(req.query.limit)?Number(req.query.limit):20;
+
+    const result=data.slice(tempOffset,tempLimit+tempOffset);
+    
+    res.status(200).send(result);
+ 
+ });
+ 
+ const senitize=(val)=>{
+     if(val===null || val===undefined || isNaN(val))
+     return false;
+ 
+     return true;
+ }
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
